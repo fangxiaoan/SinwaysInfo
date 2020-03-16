b5cfc9863e94693cf4ea31c15ac834ebdd3e4f7c
fangxiaoan <379104481@qq.com>
2020-03-16 15:07:05 +0800

    2020316 添加 添加了监听事件

--- app/src/main/java/com/sinways/sinwaysinfo/fragment/StaticFragment.java
+++ app/src/main/java/com/sinways/sinwaysinfo/fragment/StaticFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,11 +28,23 @@ public class StaticFragment extends Fragment implements View.OnClickListener {
         mView = inflater.inflate(R.layout.infofr,container,false);
         TextView tv_adv = mView.findViewById(R.id.tv_adv);
         ImageView iv_adv = mView.findViewById(R.id.iv_adv);
+        tv_adv.setOnClickListener(this);
+        iv_adv.setOnClickListener(this);
         return mView;
     }
 
     @Override
     public void onClick(View v) {
+        if(v.getId() == R.id.tv_adv){
+            showToast("您点击了文本");
+        }
+        if(v.getId() == R.id.iv_adv){
+            showToast("您点击了图片");
+        }
 
+    }
+
+    private void showToast(String msg){
+        Toast.makeText(mContext,msg,Toast.LENGTH_LONG).show();
     }
 }
